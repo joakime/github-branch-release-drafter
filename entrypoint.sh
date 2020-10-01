6c098b93c4fa3c3e68396741af0af1a817f82e34
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-10-01 11:56:24 -0500

    Making dockerfile use prebuild release

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh -l
 
-echo "Did app get populated?"
-ls -la /
-ls -la /app
+export JAVA_HOME=/opt/java/openjdk
+export PATH=$JAVA_HOME/bin:$PATH
 
-echo "Java on path?"
-which java
-
-java --version
+echo "Executing java release-drafter"
+java -cp /app/release-drafter.jar net.webtide.github.releasedrafter.Main $@
 
 echo "Args $@"
 time=$(date)
