a0371edbb0242b02b1465d1295501a0521ff8caf
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2020-10-02 11:17:11 -0500

    Quiet the build output

--- build.sh
+++ build.sh
@@ -8,12 +8,13 @@ echo "Attempting to fetch (possibly) pre-built ${BRD_URL}"
 wget -q -O ${BRD_JAR} ${BRD_URL} 2>&1 > /app/download.log
 
 if [ ! -f /app/branch-release-drafter.jar ] ; then
-  echo "Prebuilt branch-release-drafter.jar for version $BRD_VERSION not found, building afresh."
+  echo "Prebuilt branch-release-drafter.jar for version $BRD_VERSION not found, building version from source."
   export JAVA_HOME=/opt/java/openjdk
   export PATH=$JAVA_HOME/bin:$PATH
 
   cd /app/build/
-  ./mvnw clean install -e -B -V -fae -DskipTests -Dmaven.javadoc.skip=true
+  echo "Building ${BRD_JAR} ..."
+  ./mvnw clean install -q -e -B -V -fae -DskipTests -Dmaven.javadoc.skip=true
 
   cp target/branch-release-drafter.jar /app/
 fi
